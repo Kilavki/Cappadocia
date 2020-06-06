4680747e662f935668c4a4680c22e735a6b16067
kilavki <kilavki@gmail.com>
2020-06-06 17:47:10 +0300

    done: finished fairy-tale section slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,9 @@
 $(function(){
-	$('.fairy-tail__slider').slick({});
+	$('.fairy-tail__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/slides/arrow-prev.svg" alt="arrow-prev"></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/slides/arrow-next.svg" alt="arrow-next"></button>',
+		autoplay: true,
+		autoplaySpeed: 3000,
+		fade: true
+	});
 });
