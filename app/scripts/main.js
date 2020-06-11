89792de5c530e1eb91dcce0cfd99ff7d5d9a7f3c
kilavki <kilavki@gmail.com>
2020-06-11 23:49:05 +0300

    add: mobile adaptation for the section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,15 @@ $(function(){
 		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/slides/arrow-next.svg" alt="arrow-next"></button>',
 		autoplay: true,
 		autoplaySpeed: 3000,
-		fade: true
+		fade: true,
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					arrows: false,
+				}
+			}
+		]
 	});
 });
