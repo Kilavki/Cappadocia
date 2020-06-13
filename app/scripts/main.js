714c972a269162c050d8ed4ffb131441eed69621
kilavki <kilavki@gmail.com>
2020-06-13 20:08:33 +0300

    step: adaptation for mobile screens

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@ $(function(){
 		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/slides/arrow-prev.svg" alt="arrow-prev"></button>',
 		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/slides/arrow-next.svg" alt="arrow-next"></button>',
 		autoplay: true,
-		autoplaySpeed: 3000,
+		autoplaySpeed: 10000,
 		fade: true,
 		adaptiveHeight: true,
 		responsive: [
